f8aaa7f21b443a7de4a70d77d410a404910011f0
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-20 20:46:25 +0100

    feat(routing): create rest api endpoints, get, post, put, delete, head, options, and universal route

--- src/modules/Router.js
+++ src/modules/Router.js
@@ -32,4 +32,89 @@ export default class Router {
     get [Symbol.toStringTag]() {
         return 'Router';
     }
+
+    /**
+     * processes the route
+     *@param {string} routeUrl - the route's url
+     *@param {Function} callback - callback function
+     *@param {Object} [options] - optional configuration options
+     *@param {Array} [options.methods] - array of methods allowed
+     *@param {string} [overrideMethod] - a string indicating the only method allowed for the route
+     *@returns {boolean}
+    */
+    process(routeUrl, callback, options, overrideMethod) {
+        if (this.resolved)
+            return;
+    }
+
+    /**
+     * performs route rules for all http method verbs
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+     *@param {Array} [options.methods] - array of methods allowed
+    */
+    route(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, '');
+    }
+
+    /**
+     * performs route rules for only GET request method verb
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+    */
+    get(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, 'GET');
+    }
+
+    /**
+     * performs route rules for only HEAD request method verb
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+    */
+    head(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, 'HEAD');
+    }
+
+    /**
+     * performs route rules for only OPTIONS request method verb
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+    */
+    options(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, 'OPTIONS');
+    }
+
+    /**
+     * performs route rules for only DELETE method verb
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+    */
+    delete(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, 'DELETE');
+    }
+
+    /**
+     * performs route rules for only POST request method verb
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+    */
+    post(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, 'POST');
+    }
+
+    /**
+     * performs route rules for only PUT request method verb
+     *@param {string} routeUrl - the route url
+     *@param {Function} - callback function
+     *@param {Object} [options] - optional configuration options
+    */
+    put(routeUrl, callback, options) {
+        this.process(routeUrl, callback, options, 'PUT');
+    }
 }
