8b7ba0d9508fa5faf367587f504fb3ea653b84cd
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-23 01:20:51 +0100

    test(setup): refactor test setup file, add a getInstance method that beats istanbul's incorrect repo

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,21 @@
 import {expect} from 'chai';
 
 global.expect = expect;
+
+/**
+ * a workaround to istanbuls branching error on constructors that extends other constructors.
+*/
+global.getInstance = function(className, ...parameters) {
+    let proto = className.__proto__;
+
+    className.__proto__ = null;
+    try {
+        new className(...parameters);
+    }
+    catch(ex) {
+        //
+    }
+
+    className.__proto__ = proto;
+    return new className(...parameters);
+};
