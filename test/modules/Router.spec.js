ef4f0ac5ea09058b47f2b764013f7f2de8fa2d1e
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-25 13:12:26 +0100

    test: create test specifications for the Router module

---
+++ test/modules/Router.spec.js
@@ -0,0 +1,141 @@
+import Router from '../../src/modules/Router.js';
+
+describe('Router', function() {
+    let router = null;
+
+    beforeEach(function() {
+        router = new Router();
+    });
+
+    describe('#constructor(inheritMiddlewares?)', function() {
+        it(`should create a Router instance when called the optional inheritMiddlewares
+        parameter`, function() {
+            expect(router).to.be.a('Router');
+        });
+
+        it(`should set the inheritMiddlewares property to true if no parameter is given`, function() {
+            expect(router.inheritMiddlewares).to.be.true;
+        });
+
+        it(`should set the inheritMiddlewares property to the boolean parameter if given`, function() {
+            expect(new Router(false).inheritMiddlewares).to.be.false;
+        });
+
+        it(`should initialize the instance middlewares to an empty array`, function() {
+            expect(router.middlewares).to.be.an('Array').and.lengthOf(0);
+        });
+
+        it(`should initialize the instance routes to an object containing empty arrays for all
+        supported http method apis`, function() {
+            expect(router.routes).to.deep.equals({
+                options: [], head: [], get: [], post: [], put: [], delete: [], all: []});
+        });
+    });
+
+    describe('#options(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes options api store`, function() {
+            let callback = function() {}, options = {};
+            router.options('/', callback, options);
+            expect(router.routes.options).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#head(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes head api store`, function() {
+            let callback = function() {}, options = {};
+            router.head('/', callback, options);
+            expect(router.routes.head).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#get(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes get api store`, function() {
+            let callback = function() {}, options = {};
+            router.get('/', callback, options);
+            expect(router.routes.get).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#post(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes post api store`, function() {
+            let callback = function() {}, options = {};
+            router.post('/', callback, options);
+            expect(router.routes.post).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#put(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes put api store`, function() {
+            let callback = function() {}, options = {};
+            router.put('/', callback, options);
+            expect(router.routes.put).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#delete(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes delete api store`, function() {
+            let callback = function() {}, options = {};
+            router.delete('/', callback, options);
+            expect(router.routes.delete).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#all(url, callback, options?)', function() {
+        it(`should push to the given route url, callback and optional options object to the
+        routes all api store`, function() {
+            let callback = function() {}, options = {};
+            router.all('/', callback, options);
+            expect(router.routes.all).to.be.lengthOf(1).and.to.satisfy(function(routes) {
+                let route = routes[0];
+                return route[0] === '/' && route[1] === callback && route[2] === options;
+            });
+        });
+    });
+
+    describe('#route(url)', function() {
+        it(`should return a RouteWrapper instance for the given url`, function() {
+            expect(router.route('/user')).to.be.a('RouteWrapper');
+        });
+    });
+
+    describe('#use(middleware)', function() {
+        it(`should push the given middleware callback to the instance middlewares array`, function() {
+            let middleware = function() {};
+            router.use(middleware);
+
+            expect(router.middlewares).to.be.lengthOf(1).and.satisfy(function(middlewares) {
+                return middlewares[0] === middleware;
+            });
+        });
+
+        it(`should do nothing if argument is not a callable`, function() {
+            router.use({middleware: () => {}});
+
+            expect(router.middlewares).to.be.lengthOf(0);
+        });
+    });
+});
