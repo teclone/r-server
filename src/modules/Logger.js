f0153eb62e4b582fccb41a36dab27b0620a1c434
Harrison Ifeanyichukwu <harrisonifeanyichukwu@gmail.com>
2018-07-26 14:56:19 +0100

    feat: add a logProfile method that logs the profiled request response, time, method, response status

--- src/modules/Logger.js
+++ src/modules/Logger.js
@@ -14,4 +14,18 @@ export default {
     info(message) {
         console.log('\x1b[1m\x1b[32m%s\x1b[0m', message);
     },
+
+    /**
+     * log request response profile
+     *@param {http.IncomingMessage} req - the request object
+     *@param {RServerResponse} res - the response object
+    */
+    logProfile(req, res) {
+        if (res.statusCode >= 400)
+            console.log('%s: %s \x1b[31m%d\x1b[0m ~%dms ~%dms\x1b[0m', req.method, req.url,
+                res.statusCode, res.startTime - req.startTime, Date.now() - res.startTime);
+        else
+            console.log('%s: %s \x1b[32m%d\x1b[0m ~%dms ~%dms\x1b[0m', req.method, req.url,
+                res.statusCode, res.startTime - req.startTime, Date.now() - res.startTime);
+    }
 };
