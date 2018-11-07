5affb7e0ca37a214a6d1567ef662666c7c77f74f
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2018-11-07 09:53:55 +0100

    chore: upgrade project dev dependencies, integrate sinon, update eslint to target es2018, and update

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import {uglify} from 'rollup-plugin-uglify';
 
 import rollupAll from 'rollup-all';
 
-export default rollupAll.getExports(uglify(), [
+let plugins = [
     resolve(),
     babel({
-        exclude: 'node_modules/**',
-        plugins: ['external-helpers']
+        exclude: 'node_modules/**'
     })
-]);
+];
+
+export default rollupAll.getExports(null, plugins);
