8c35be6c970f01b05f8271f2628b00b89db63fea
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2018-11-07 10:28:43 +0100

    refactor: simplify callback using the new json method on the response object

--- server.js
+++ server.js
@@ -2,26 +2,25 @@ let RServer = require('./lib/main.js');
 
 let app = RServer.instance();
 
-app.listen(); //listens on port 8131 by default
-
 app.use(function(req, res, next) {
     next();
 });
 
 /**process form upload from our examples index.html file*/
 app.post('process-upload', function(req, res) {
-
-    res.writeHead(200, {'Content-Type': 'application/json'});
-
     let result = JSON.stringify(
-        Object.assign({}, req.body, req.files)
+        Object.assign(
+            {},
+            req.body,
+            req.files
+        )
     );
-
-    //beautify json before sending
-    res.end(require('js-beautify').js(result));
+    res.json(require('js-beautify').js(result));
 });
 
 //send download
 app.get('send-download', (req, res) => {
     res.download('package.json');
 });
+
+app.listen(); //listens on port 4000 by default
