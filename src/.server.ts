d3e5ed37642cfdcc4449edcae04e0c8d799860a9
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-04-29 12:33:15 +0100

    refactor: refactor server config file, and test main export

---
+++ src/.server.ts
@@ -0,0 +1,57 @@
+import { RServerConfig } from './@types';
+
+const rServerConfig: RServerConfig = {
+
+    env: 'dev',
+
+    errorLog: '.log/error.log',
+
+    accessLog: '.log/access.log',
+
+    profileRequest: true,
+
+    tempDir: 'storage/temp',
+
+    publicPaths: [
+        'public'
+    ],
+
+    serveHiddenFiles: false,
+
+    cacheControl: 'no-cache, max-age=86400',
+
+    encoding: 'latin1',
+
+    maxMemory: 50000000,
+
+    defaultDocuments: [
+        'index.html',
+        'index.js',
+        'index.css'
+    ],
+
+    httpErrors: {
+        baseDir: '',
+        404: '',
+        500: ''
+    },
+
+    https: {
+        enabled: false,
+        /* can be overriden by setting process.env.HTTPS_PORT */
+        port: 9000,
+
+        /* enforce https by redirecting all http request to https */
+        enforce: true,
+
+        /* https credentials, use  */
+        credentials: {
+            key: '.cert/server.key',
+            cert: '.cert/server.crt',
+            //'pfx': 'relativePath',
+            passphrase: 'pfx passphrase'
+        }
+    }
+}
+
+export default rServerConfig;
