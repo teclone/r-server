8dc39d9206f97091e68896c68756d4f21d41be9c
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-04-30 10:37:16 +0100

    chore: update chore files, modify script files

--- scripts/gen-ssl-cert.sh
+++ scripts/gen-ssl-cert.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "changin file access permission for test purposes"
-chmod u=wx public/write-only.txt
-
 subj="/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=localhost"
 out_dir=".cert"
 
@@ -9,8 +6,9 @@ out_dir=".cert"
 
 cd $out_dir
 
-echo "generating certificates"
+echo "generating ssl self signed certificates for https testing"
+
 openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "$subj"
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 
-echo completed;
+echo "generated ssl self signed certificates for https testing"
