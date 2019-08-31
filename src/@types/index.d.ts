14124fff78dfc89517eaa99e993211994ff505af
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-08-31 22:51:36 +0100

    feat: make middleware and callback req and res objects extendable

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -3,183 +3,207 @@ import Request from '../modules/Request';
 import Response from '../modules/Response';
 
 export interface RServerConfig {
+  env: 'dev' | 'prod';
 
-    env: 'dev' | 'prod';
+  errorLog: string;
 
-    errorLog: string;
+  accessLog: string;
 
-    accessLog: string;
+  profileRequest: boolean;
 
-    profileRequest: boolean;
+  tempDir: string;
 
-    tempDir: string;
+  publicPaths: string[];
 
-    publicPaths: string[];
+  serveHiddenFiles: boolean;
 
-    serveHiddenFiles: boolean;
+  cacheControl: string;
 
-    cacheControl: string;
+  encoding: string;
 
-    encoding: string;
+  maxMemory: string | number;
 
-    maxMemory: string | number;
+  defaultDocuments: string[];
 
-    defaultDocuments: string[];
+  httpErrors: {
+    baseDir: string;
 
-    httpErrors: {
-        baseDir: string;
+    404: string;
 
-        404: string;
+    500: string;
+  };
 
-        500: string;
-    }
+  https: {
+    enabled: boolean;
 
-    https: {
-        enabled: boolean;
+    port: number;
 
-        port: number;
+    enforce: boolean;
 
-        enforce: boolean;
-
-        credentials: {key: string; cert: string;} | { pfx: string; passphrase: string}
-    }
+    credentials:
+      | { key: string; cert: string }
+      | { pfx: string; passphrase: string };
+  };
 }
 
 export interface Config {
+  env?: 'dev' | 'prod';
 
-    env?: 'dev' | 'prod';
+  errorLog?: string;
 
-    errorLog?: string;
+  accessLog?: string;
 
-    accessLog?: string;
+  profileRequest?: boolean;
 
-    profileRequest?: boolean;
+  tempDir?: string;
 
-    tempDir?: string;
+  publicPaths?: string[];
 
-    publicPaths?: string[];
+  serveHiddenFiles?: boolean;
 
-    serveHiddenFiles?: boolean;
+  cacheControl?: string;
 
-    cacheControl?: string;
+  encoding?: string;
 
-    encoding?: string;
+  maxMemory?: string | number;
 
-    maxMemory?: string | number;
+  defaultDocuments?: string[];
 
-    defaultDocuments?: string[];
+  httpErrors?: {
+    baseDir?: string;
 
-    httpErrors?: {
-        baseDir?: string;
+    404: string;
 
-        404: string;
+    500?: string;
+  };
 
-        500?: string;
-    }
+  https?: {
+    enabled: boolean;
 
-    https?: {
-        enabled: boolean;
+    port?: number;
 
-        port?: number;
+    enforce?: boolean;
 
-        enforce?: boolean;
-
-        credentials?: {key: string; cert: string;} | { pfx: string; passphrase: string}
-    }
+    credentials?:
+      | { key: string; cert: string }
+      | { pfx: string; passphrase: string };
+  };
 }
 
-export declare type Method = 'get' | 'post' | 'put' | 'head' | 'options' | 'delete' | 'all';
+export type Method =
+  | 'get'
+  | 'post'
+  | 'put'
+  | 'head'
+  | 'options'
+  | 'delete'
+  | 'all';
 
-export declare type Url = string;
+export type Url = string;
 
-export declare type Parameter = string | number | boolean;
+export type Parameter = string | number | boolean;
 
-export declare type Next = () => void;
+export type Next = () => void;
 
-export declare type Callback = (request: Request, response: Response,
-    ...parameters: Parameter[]) => Promise<boolean>;
+export type Callback = <
+  Req extends Request = Request,
+  Res extends Response = Response
+>(
+  request: Req,
+  response: Res,
+  ...parameters: Parameter[]
+) => Promise<boolean>;
 
-export declare type Middleware = (request: Request, response: Response, next: Next,
-    ...parameters: Parameter[]) => void;
+export type Middleware = <
+  Req extends Request = Request,
+  Res extends Response = Response
+>(
+  request: Req,
+  response: Res,
+  next: Next,
+  ...parameters: Parameter[]
+) => void;
 
-export declare type ListenerCallback = () => void;
+export type ListenerCallback = () => void;
 
-
-export declare interface CallbackOptions {
-    middleware: Middleware | Middleware[];
+export interface CallbackOptions {
+  middleware: Middleware | Middleware[];
 }
 
-export declare interface MiddlewareOptions {
-    method: Method | Method[];
+export interface MiddlewareOptions {
+  method: Method | Method[];
 }
 
-export declare interface ResolvedCallbackOptions {
-    middleware: Middleware[]
+export interface ResolvedCallbackOptions {
+  middleware: Middleware[];
 }
 
-export declare interface ResolvedMiddlewareOptions {
-    method: Method[]
+export interface ResolvedMiddlewareOptions {
+  method: Method[];
 }
 
-export declare type RouteInstance = [Url, Callback, null | ResolvedCallbackOptions];
+export type RouteInstance = [Url, Callback, null | ResolvedCallbackOptions];
 
-export declare type MiddlewareInstance = [Url, Middleware[], null | ResolvedMiddlewareOptions];
+export type MiddlewareInstance = [
+  Url,
+  Middleware[],
+  null | ResolvedMiddlewareOptions
+];
 
-
-export declare interface File {
-    name: string;
-    tmpName: string;
-    path: string;
-    size: number;
-    type: string;
+export interface File {
+  name: string;
+  tmpName: string;
+  path: string;
+  size: number;
+  type: string;
 }
 
-export declare interface FileCollection {
-    name: string[];
-    tmpName: string[];
-    path: string[];
-    size: number[];
-    type: string[];
+export interface FileCollection {
+  name: string[];
+  tmpName: string[];
+  path: string[];
+  size: number[];
+  type: string[];
 }
 
-export declare interface Files {
-    [fieldName: string]: File | FileCollection
+export interface Files {
+  [fieldName: string]: File | FileCollection;
 }
 
-export declare interface Data {
-    [propName: string]: string | string[]
+export interface Data {
+  [propName: string]: string | string[];
 }
 
-export declare interface Headers {
-    [propName: string]: string;
+export interface Headers {
+  [propName: string]: string;
 }
 
-export declare interface Range {
-    start: number;
-    end: number;
-    length: number;
+export interface Range {
+  start: number;
+  end: number;
+  length: number;
 }
 
-export declare interface MultipartHeaders {
-    isFile: boolean;
-    fileName: string;
-    fieldName: string;
-    encoding: string;
-    type: string
+export interface MultipartHeaders {
+  isFile: boolean;
+  fileName: string;
+  fieldName: string;
+  encoding: string;
+  type: string;
 }
 
-export declare interface RouteParameter {
-    name: string;
-    dataType: string;
-    value: string | number | boolean;
+export interface RouteParameter {
+  name: string;
+  dataType: string;
+  value: string | number | boolean;
 }
 
-export declare interface Routes {
-    options: RouteInstance[];
-    head: RouteInstance[];
-    get: RouteInstance[];
-    post: RouteInstance[];
-    put: RouteInstance[];
-    delete: RouteInstance[];
-    all: RouteInstance[]
+export interface Routes {
+  options: RouteInstance[];
+  head: RouteInstance[];
+  get: RouteInstance[];
+  post: RouteInstance[];
+  put: RouteInstance[];
+  delete: RouteInstance[];
+  all: RouteInstance[];
 }
