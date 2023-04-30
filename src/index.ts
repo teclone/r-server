5773c3989be2d5adca2190f6901d5903fdaebd90
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2023-04-30 08:38:19 +0200

    feat: expose request and response classes for extensibility

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import { RServerConfig } from './@types';
 export { Server } from './modules/Server';
 export { Router } from './modules/Router';
 
+export { Http1Request, Http2Request } from './modules/Request';
+export { Http1Response, Http2Response } from './modules/Response';
+
 export const createConfig = (config: RServerConfig) => config;
